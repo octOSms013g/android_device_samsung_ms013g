b54ac79d4cc87faef47ea1a374bcb22f611d28d8
Aditya Upreti <aupreti72@gmail.com>
2016-05-30 09:39:00 +0530

    Tipsy-ify

--- tipsy.mk
+++ tipsy.mk
@@ -12,8 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common Bliss stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/ms013g/full_ms013g.mk)
@@ -21,7 +25,7 @@ $(call inherit-product, device/samsung/ms013g/full_ms013g.mk)
 # Release name
 PRODUCT_RELEASE_NAME := SM-G7102
 PRODUCT_DEVICE := ms013g
-PRODUCT_NAME := aosp_ms013g
+PRODUCT_NAME := tipsy_ms013g
 
 # Product property
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ms013gxx TARGET_DEVICE=ms013g BUILD_FINGERPRINT="samsung/ms013gxx/ms013g:4.4.2/KOT49H/G7102XXUBOB1:user/release-keys" PRIVATE_BUILD_DESC="ms013gxx-user 4.4.2 KOT49H G7102XXUBOB1 release-keys"
