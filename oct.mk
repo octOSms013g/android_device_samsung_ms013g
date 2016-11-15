0ba81d66689f2fdbe206aa6e6b6cff858f0f6f16
SwapnilBunny <Yenkule@gmail.com>
2016-11-15 20:30:43 +0530

    Update and rename bliss.mk to oct.mk

--- oct.mk
+++ oct.mk
@@ -16,12 +16,12 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/ms013g/full_ms013g.mk)
+$(call inherit-product, device/samsung/ms013g/oct_ms013g.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := SM-G7102
 PRODUCT_DEVICE := ms013g
-PRODUCT_NAME := cm_ms013g
+PRODUCT_NAME := oct_ms013g
 
 # Product property
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ms013gxx TARGET_DEVICE=ms013g BUILD_FINGERPRINT="samsung/ms013gxx/ms013g:4.4.2/KOT49H/G7102XXUBOB1:user/release-keys" PRIVATE_BUILD_DESC="ms013gxx-user 4.4.2 KOT49H G7102XXUBOB1 release-keys"
